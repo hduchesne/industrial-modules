d597f6679872630e27785236b1615300d3d3464e
Hervé Duchesne <hduchesne@jahia.com>
2020-05-25 19:30:15 +0200

    Bug fix, groovy menu is now working

--- industrial-components/src/main/resources/bootstrap4nt_navbar/html/navbar.industrial.basenav.groovy
+++ industrial-components/src/main/resources/bootstrap4nt_navbar/html/navbar.industrial.basenav.groovy
@@ -7,8 +7,11 @@ import org.slf4j.LoggerFactory
 
 import javax.jcr.ItemNotFoundException
 
+logger = LoggerFactory.getLogger(this.class)
 
 getPagesL1={value,curentPageNode ->
+    logger.info("value :"+value.toString())
+    logger.info("curentPageNode :"+curentPageNode.toString())
     switch (value){
         case "currentPage":return JCRTagUtils.getChildrenOfType(curentPageNode, 'jmix:navMenuItem')
         case "parentPage":return JCRTagUtils.getChildrenOfType(curentPageNode.parent, 'jmix:navMenuItem')
@@ -32,7 +35,7 @@ getUrl={node ->
     }
 }
 
-showPage{node ->
+showPage={node ->
     if(!node.properties['j:displayInMenuName'])
         return true
 
@@ -43,8 +46,21 @@ showPage{node ->
     return show
 }
 
-createDropdown={node,children ->
-    def html = '''\
+def li_navItemHTML = '''
+        <li class="nav-item ${active ? ' active':''}">
+            <a class="nav-link" href="${url}">
+                ${title}
+                ${active ?'<span class="sr-only">(current)</span>':''}
+            </a>
+        </li>
+        '''
+def a_dropdownItemHTML = '''
+        <a class="dropdown-item ${active ? ' active':''}" href="${url}">
+            ${title}
+            ${active ?'<span class="sr-only">(current)</span>':''}
+        </a>
+        '''
+def li_dropdownHTML = '''
         <li class="nav-item  ${active? ' active' :''} dropdown">
             <a class="nav-link dropdown-toggle ${active? ' active' :''}" href="#"
                id="navbarDropdownMen-${identifier}"
@@ -55,76 +71,60 @@ createDropdown={node,children ->
                  aria-labelledby="navbarDropdownMen-${identifier}">
                 <a class="dropdown-item" href="${url}">${title}</a>
                 <div class="dropdown-divider"></div>
-                ${children.each {node -> createDropdownItem(node)} }
-            </div>
-        </li>
         '''
-    def template = new groovy.text.StreamingTemplateEngine().createTemplate(html)
-    def binding = [
-            active : renderContext.mainResource.path.contains(node.path),
-            url:getUrl(node),
-            title:getTitle(node),
-            identifier:node.identifier,
-            children:children
-    ]
-    return template.make(binding)
-}
-createDropdownItem={ node ->
-    def html = '''\
-        <a class="dropdown-item ${active ? ' active':''}" href="${url}">
-            ${title}
-            ${active ?'<span class="sr-only">(current)</span>':''}
-        </a>'''
-    def template = new groovy.text.StreamingTemplateEngine().createTemplate(html)
-    def binding = [
-            active : renderContext.mainResource.path.contains(node.path),
-            url:getUrl(node),
-            title:getTitle(node)
-    ]
-    return template.make(binding)
-}
-createNavItem={node ->
-    def html = '''\
-        <li class="nav-item ${active ? ' active':''}">\
-            <a class="nav-link" href="${url}">
-                ${title}
-                ${active ?'<span class="sr-only">(current)</span>':''}
-            </a>
-        </li>'''
-    def template = new groovy.text.StreamingTemplateEngine().createTemplate(html)
-    def binding = [
-            active : renderContext.mainResource.path.contains(node.path),
-            url:getUrl(node),
-            title:getTitle(node)
-    ]
-    return template.make(binding)
-}
+def ulHTML = '''    
+        <ul class="${classUL}">
+        '''
+
+def ul = new groovy.text.StreamingTemplateEngine().createTemplate(ulHTML)
+def li_dropdown = new groovy.text.StreamingTemplateEngine().createTemplate(li_dropdownHTML)
+def a_dropdownItem = new groovy.text.StreamingTemplateEngine().createTemplate(a_dropdownItemHTML)
+def li_navItem = new groovy.text.StreamingTemplateEngine().createTemplate(li_navItemHTML)
 
 createNav = { node,recursive ->
     if(!showPage(node))
         return
 
     def childNodes = JCRTagUtils.getChildrenOfType(node, 'jmix:navMenuItem')
-    if(childNodes && recursive)
-        return createDropdownItem(node,childNodes)
+    logger.debug("childNodes : "+childNodes.toString())
+    logger.debug("recursive : "+recursive.toString())
 
-    return createNavItem(node)
+    if(childNodes && recursive){
+
+        print li_dropdown.make([
+                active : renderContext.mainResource.path.contains(node.path),
+                url:getUrl(node),
+                title:getTitle(node),
+                identifier:node.identifier
+        ])
+
+        childNodes.each {childNode ->
+            print a_dropdownItem.make([
+                    active : renderContext.mainResource.path.contains(childNode.path),
+                    url:getUrl(childNode),
+                    title:getTitle(childNode)
+            ])
+        }
+
+        print "</div></li>"
+    }else{
+        print li_navItem.make([
+            active : renderContext.mainResource.path.contains(node.path),
+            url:getUrl(node),
+            title:getTitle(node)
+        ])
+    }
 }
 
 curentPageNode = JCRTagUtils.isNodeType(renderContext.mainResource.node,'jmix:navMenuItem')?
         renderContext.mainResource.node :
         JCRTagUtils.getParentOfType(renderContext.mainResource.node,'jmix:navMenuItem')
 
-classUL = JCRTagUtils.isNodeType(currentNode, 'bootstrap4mix:customBaseNavbar') ?
-        currentNode.properties.ulClass.string : null
-
-recursive = JCRTagUtils.isNodeType(currentNode, 'bootstrap4mix:customBaseNavbar') ?
-        currentNode.properties.recursive.string : false
-
 def pagesL1 = getPagesL1(
         currentNode.properties.root.string,
         curentPageNode
 )
+logger.debug("pagesL1 : "+pagesL1.toString())
 
 // Add dependencies to parent of main resource so that we are aware of new pages at sibling level
 try {
@@ -133,14 +133,14 @@ try {
 }
 
 if(!pagesL1.isEmpty()){
-    def html = '''     
-        <ul class="${classUL}">
-            ${pages.each {page -> createNav(page,recursive))}}
-        </ul>
-        '''
-    def template = new groovy.text.StreamingTemplateEngine().createTemplate(html)
+    def recursive=JCRTagUtils.isNodeType(currentNode, 'bootstrap4mix:customBaseNavbar') ?
+            currentNode.properties.recursive.string : false
+    
     def binding = [
-            pages : pagesL1
+            classUL:JCRTagUtils.isNodeType(currentNode, 'bootstrap4mix:customBaseNavbar') ?
+                    currentNode.properties.ulClass.string : null
     ]
-    return template.make(binding)
+    print ul.make(binding)
+    pagesL1.each {page -> createNav(page,recursive) }
+    print"</ul>"
 }
